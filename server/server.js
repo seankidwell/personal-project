72f1c7c0eddd4b314afdec90fd7e69365698271b
seankidwell <dev.seankidwell@gmail.com>
2018-08-22 18:03:01 -0600

    login fixed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const massive = require("massive");
 const axios = require("axios");
-const aws = require('aws-sdk')
+const aws = require("aws-sdk");
 const controller = require("./controller");
 const path = require("path");
 require("dotenv").config();
@@ -11,16 +11,17 @@ require("dotenv").config();
 const app = express();
 const port = 3090;
 
-app.use(express.static(__dirname+'/../build'));
+app.use(express.static(__dirname + "/../build"));
 
 let {
   REACT_APP_CLIENT_ID,
   CLIENT_SECRET,
   REACT_APP_DOMAIN,
   CONNECTION_STRING,
-  SESSION_SECRET
+  SESSION_SECRET,
+  NODE_ENV
 } = process.env;
-
+console.log("the node environment is:", NODE_ENV);
 app.use(bodyParser.json());
 app.use(
   session({
@@ -34,84 +35,83 @@ massive(CONNECTION_STRING).then(db => {
   app.set("db", db);
 });
 
-const {
-    S3_BUCKET,
-    AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY
-} = process.env
+const { S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
 
-app.get('/api/sign-s3', (req, res) => {
-  console.log('get hit')
+app.get("/api/sign-s3", (req, res) => {
+  console.log("get hit");
 
   aws.config = {
-    region: 'us-west-1',
+    region: "us-west-1",
     accessKeyId: AWS_ACCESS_KEY_ID,
     secretAccessKey: AWS_SECRET_ACCESS_KEY
-  }
-  
+  };
+
   const s3 = new aws.S3();
-  const fileName = req.query['file-name'];
-  const fileType = req.query['file-type'];
+  const fileName = req.query["file-name"];
+  const fileType = req.query["file-type"];
   const s3Params = {
     Bucket: S3_BUCKET,
     Key: fileName,
     Expires: 60,
     ContentType: fileType,
-    ACL: 'public-read'
+    ACL: "public-read"
   };
 
-  s3.getSignedUrl('putObject', s3Params, (err, data) => {
-    if(err){
+  s3.getSignedUrl("putObject", s3Params, (err, data) => {
+    if (err) {
       console.log(err);
       return res.end();
     }
-    console.log(data)
+    console.log(data);
     const returnData = {
       signedRequest: data,
       url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
     };
 
-    return res.send(returnData)
+    return res.send(returnData);
   });
 });
 
 app.get("/auth/callback", async (req, res) => {
-  console.log('auth sucks')
-  try{
-  let payload = {
-    client_id: REACT_APP_CLIENT_ID,
-    client_secret: CLIENT_SECRET,
-    code: req.query.code,
-    grant_type: "authorization_code",
-    redirect_uri: `http://${req.headers.host}/auth/callback`
-  };
-  console.log('token')
-  let responseWithToken = await axios.post(
-    `https://${REACT_APP_DOMAIN}/oauth/token`,
-    payload
-  );
-  console.log('user')
-  let userData = await axios.get(
-    `https://${REACT_APP_DOMAIN}/userinfo?access_token=${
-      responseWithToken.data.access_token
-    }`
-  );
+  console.log("auth sucks");
+  try {
+    let payload = {
+      client_id: REACT_APP_CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      code: req.query.code,
+      grant_type: "authorization_code",
+      redirect_uri:
+        NODE_ENV === "production"
+          ? `https://${req.headers.host}/auth/callback`
+          : `http://${req.headers.host}/auth/callback`
+    };
+    console.log("token");
+    let responseWithToken = await axios.post(
+      `https://${REACT_APP_DOMAIN}/oauth/token`,
+      payload
+    );
+    console.log("user");
+    let userData = await axios.get(
+      `https://${REACT_APP_DOMAIN}/userinfo?access_token=${
+        responseWithToken.data.access_token
+      }`
+    );
 
-  const db = req.app.get("db");
-  let { sub, name, picture } = userData.data;
-  console.log('finding user')
-  let userExists = await db.find_user([sub]);
-  if (userExists[0]) {
-    req.session.user = userExists[0];
-    res.redirect(`${process.env.FRONTEND_DOMAIN}/#/`);
-  } else {
-    db.create_user([sub, name, picture]).then(createdUser => {
-      req.session.user = createdUser[0];
+    const db = req.app.get("db");
+    let { sub, name, picture } = userData.data;
+    console.log("finding user");
+    let userExists = await db.find_user([sub]);
+    if (userExists[0]) {
+      req.session.user = userExists[0];
       res.redirect(`${process.env.FRONTEND_DOMAIN}/#/`);
-    });
-  }
-  } catch(err) {
-    console.error('auth callback failed', err)
+    } else {
+      db.create_user([sub, name, picture]).then(createdUser => {
+        req.session.user = createdUser[0];
+        res.redirect(`${process.env.FRONTEND_DOMAIN}/#/`);
+      });
+    }
+  } catch (err) {
+    console.error("auth callback failed", err);
   }
 });
 
@@ -138,8 +138,8 @@ app.put("/api/forum/comment/:commentId", controller.editComment);
 app.post("/api/forum/comments/:postId", controller.createComment);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/../build/index.html`))
-})
+  res.sendFile(path.resolve(`${__dirname}/../build/index.html`));
+});
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
